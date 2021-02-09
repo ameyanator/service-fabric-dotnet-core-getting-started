2ecf8e9263724016bec96aa0a3da20700719639f
Ameya Sinha <ameyaosd@gmail.com>
2021-02-09 12:55:12 +0530

    fix incorrect path

--- Actors/CalculatorActor/replaceDlls.sh
+++ Actors/CalculatorActor/replaceDlls.sh
@@ -1,9 +1,9 @@
-rm ./System.Fabric.McgInterop.dll
-rm ./Microsoft.ServiceFabric.ReliableCollection.Interop.dll
-rm ./Microsoft.ServiceFabric.FabricTransport.McgInterop.dll
-rm ./Microsoft.ServiceFabric.Internal.McgInterop.dll
-rm ./System.Private.Interop.dll
-rm ./Microsoft.ServiceFabric.FabricTransport.V2.McgInterop.dll
+rm CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0/System.Fabric.McgInterop.dll
+rm CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0/Microsoft.ServiceFabric.ReliableCollection.Interop.dll
+rm CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0/Microsoft.ServiceFabric.FabricTransport.McgInterop.dll
+rm CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0/Microsoft.ServiceFabric.Internal.McgInterop.dll
+rm CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0/System.Private.Interop.dll
+rm CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0/Microsoft.ServiceFabric.FabricTransport.V2.McgInterop.dll
 
 cp ~/depot/WindowsFabric/net5dlls/System.Fabric.McgInterop.dll CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0
 cp ~/depot/WindowsFabric/net5dlls/Microsoft.ServiceFabric.ReliableCollection.Interop.dll CalculatorActorApplication/CalculatorActorPkg/Code/runtimes/linux/lib/netstandard2.0
